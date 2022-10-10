4e2b06e8643545bbacd91142e71e04e198ab1373
pozner2000 <83282528+pozner2000@users.noreply.github.com>
2022-10-10 15:22:26 +0300

    Delete form.php

--- form.php
+++
@@ -1,8 +0,0 @@
-<form action="">
-	<input type="text" name="a">
-	<input type="text" name="b">
-	<input type="text" name="n">
-	<input type="text" name="k">
-    <input type="checkbox" name="c" value="1">
-	<input type="submit">
-</form>
