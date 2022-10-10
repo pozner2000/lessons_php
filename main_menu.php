6c2f5a948118621aefc582ee084f77d2da95b826
pozner2000 <83282528+pozner2000@users.noreply.github.com>
2022-10-10 15:23:14 +0300

    Delete main_menu.php

--- main_menu.php
+++
@@ -1,7 +0,0 @@
-<ul>
-    <li><a href="/index.php/ladia"> LADIA </a></li>
-    <li><a href="/index.php/slon"> SLON </a></li>
-    <li><a href="/index.php/sum"> SUM </a></li>
-    <li><a href="/index.php/test"> TEST </a></li>
-    <li><a href="/index.php/error"> ERROR </a> </li>
-</ul>
